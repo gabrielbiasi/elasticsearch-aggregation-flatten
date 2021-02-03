0cffa6f7b639cb900ec4b8ae6414cca26bfe4dfc
Gabriel de Biasi <biasi131@gmail.com>
2021-02-02 22:21:19 -0400

    Fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="elasticsearch-aggregation-flatten",
-    version="0.0.2",
+    version="0.0.3",
     author="Gabriel de Biasi",
     author_email="biasi131@gmail.com",
     description="This python package helps to process an aggregation response from Elasticsearch and output it as easy-to-use format, such as json and csv.",
